ba547e974914abdf573d6f2bf9a5660f0db3eebb
Shadboy9625 <shadabkalim375@gmail.com>
2024-01-30 15:44:10 +0530

    done and dusted

---
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -0,0 +1,10 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
+  const { userId } = auth();
+
+  return <>{children}</>;
+};
+
+export default TeacherLayout;
